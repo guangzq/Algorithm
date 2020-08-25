75d474b5d0c868e18833c6aa1972e90f1d28490e
zhuqiguang <qiguang.zhu@foxmail.com>
2020-08-25 17:52:48 +0800

    添加3. 无重复字符的最长子串

--- md/sort.md
+++ md/sort.md
@@ -6,6 +6,9 @@
 滑动窗口指的是利用双指针间距作为一个窗口，两个指针的移动可以为解决问题创造条件
 ### 一. [239. 滑动窗口最大值](https://leetcode-cn.com/problems/sliding-window-maximum/)
 ### 二.[3. 无重复字符的最长子串](https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/)
+定义两个指针start和end
+1. 如果map中包含key，则将start指针移动key对应的最大value
+2. 更新ans
 ### 三.[209. 长度最小的子数组](https://leetcode-cn.com/problems/minimum-size-subarray-sum/)
 定义两个指针left和right,
 1. right右移，并记录之前的累加之和
---
+++ src/Solution_3.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution_3 {
+    /**
+     * 3. 无重复字符的最长子串
+     * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) return 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0,  ans =0;
+        for (int end=0;end<s.length();end++) {
+            char aChar = s.charAt(end);
+            if (map.containsKey(aChar)) {
+                start = Math.max(start, map.get(aChar));
+            }
+            map.put(aChar, end + 1);
+            ans = Math.max(ans, end - start + 1);
+        }
+        return ans;
+    }
+}
